422cc8a2951672aaed1f45d28a46ec2342f82562
Yves Lavoie <ylavoie@yveslavoie.com>
2017-09-21 15:01:17 -0400

    Patch docker ndots sickness

--- start.sh
+++ start.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-pushd ~
-  mkdir -p .config/mc
-  tar Jxf mcthemes.tar.xz
-  ./mcthemes/mc_change_theme.sh mcthemes/puthre.theme
-popd
-
 update_ssmtp.sh
 cd /srv/ledgersmb
-
-# Revalidate the required CPAN packages in case the
-# source tree is bind mounted and has changed since Docker build.
-#cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
-#cpanm --with-develop HTTP::AcceptLanguage
 
 if [[ ! -f ledgersmb.conf ]]; then
   cp conf/ledgersmb.conf.default ledgersmb.conf
@@ -32,14 +21,18 @@ fi
 #   --postgres_password "$POSTGRES_PASS"
 #fi
 
+# Patch Docker ndots:0 sickness
+sudo chmod 666 /etc/resolv.conf
+echo "options ndots:1" >>/etc/resolv.conf
+sudo chmod 644 /etc/resolv.conf
+
 # start ledgersmb
 if [[ ! -f DEVELOPMENT ]]; then
 #  exec plackup --port 5001 --server Starman tools/starman.psgi \
   exec plackup --port 5001 --server HTTP::Server::PSGI tools/starman.psgi \
-      --Reload "lib, old/lib, xt/lib, /usr/local/share/perl, /usr/share/perl, /usr/share/perl5"
+      --Reload "lib, old/lib, xt/lib, t, xt, /usr/local/share/perl, /usr/share/perl, /usr/share/perl5"
 else
   exec plackup --port 5001 --server HTTP::Server::PSGI tools/starman-development.psgi \
       --workers 1 --env development \
-      --Reload "lib, old/lib, xt/lib, /usr/local/share/perl, /usr/share/perl, /usr/share/perl5"
+      --Reload "lib, old/lib, xt/lib, t, xt, /usr/local/share/perl, /usr/share/perl, /usr/share/perl5"
 fi
-
