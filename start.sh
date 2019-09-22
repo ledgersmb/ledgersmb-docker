52fdf5b65e7f4ea11cfda7f21cdbd3249a9599fa
Erik Huelsmann <ehuels@gmail.com>
2019-09-22 22:56:51 +0200

    Address failure to start Docker image
    Starman is looking for LedgerSMB::PSGI and can't find it due to missing
    library paths. Add -I arguments to fix it up.

--- start.sh
+++ start.sh
@@ -20,30 +20,14 @@ EOF
   export LSMB_CONFIG_FILE='/tmp/ledgersmb.conf'
 fi
 
-if [ ! -f "/tmp/ledgersmb" ]; then
-  mkdir /tmp/ledgersmb
+if [ ! -d "/tmp/ledgersmb" ]; then
+  mkdir -p /tmp/ledgersmb
 fi
-# Currently unmaintained/untested
-# if [ ! -z ${CREATE_DATABASE+x} ]; then
-#   perl tools/dbsetup.pl --company $CREATE_DATABASE \
-#   --host $POSTGRES_HOST \
-#   --postgres_password "$POSTGRES_PASS"
-#fi
-
-# Needed for modules loaded by cpanm
-export PERL5LIB
-
-for PerlLib in /usr/lib/perl5* /usr/local/lib/perl5*/site_perl/* ; do
-    [[ -d "$PerlLib" ]] && {
-        PERL5LIB="$PerlLib";
-        echo -e "\tmaybe: $PerlLib";
-    }
-done ;
-echo "Selected PERL5LIB=$PERL5LIB";
 
 # start ledgersmb
 # --preload-app allows application initialization to kill the entire
 # starman instance (instead of just the worker, which will immediately
 # get restarted); it also has a positive effect on memory use
 
-exec starman --port 5762 --workers ${LSMB_WORKERS:-5} --preload-app bin/ledgersmb-server.psgi
+exec starman --port 5762 --workers ${LSMB_WORKERS:-5} -I lib -I old/lib \
+        --preload-app bin/ledgersmb-server.psgi
