943fa166e8bab3a55fda118631d0224816fbd005
Erik Huelsmann <ehuels@gmail.com>
2021-09-12 22:19:04 +0200

    Update start.sh
    The starman option `max-workers` actually does not exist...

--- start.sh
+++ start.sh
@@ -100,6 +100,6 @@ echo '--------- LEDGERSMB CONFIGURATION --- END'
 
 # ':5762:' suppresses an uninitialized variable warning in starman
 # the last colon means "don't connect using tls"; without it, there's a warning
-exec starman --listen :5762: --max-workers ${LSMB_WORKERS:-5} \
+exec starman --listen :5762: --workers ${LSMB_WORKERS:-5} \
              -I lib -I old/lib \
              --preload-app bin/ledgersmb-server.psgi
