690fac08beab09a782fa9de43b7db6137efb9cee
Erik Huelsmann <ehuels@gmail.com>
2021-09-17 22:35:04 +0200

    Add reverse proxy configuration option

--- start.sh
+++ start.sh
@@ -100,6 +100,8 @@ ${LSMB_MAIL_SMTPHOST:+smtphost=$LSMB_MAIL_SMTPHOST
 }${LSMB_MAIL_SMTPAUTHMECH:+smtpauthmech=$LSMB_MAIL_SMTPAUTHMECH
 }
 
+[proxy]
+ip=${PROXY_IP:-172.17.0.1/16}
 EOF
   export LSMB_CONFIG_FILE='/tmp/ledgersmb.conf'
 fi
