70c3da39183a152784219ecf5fdb3f2ebf1c2961
Erik Huelsmann <ehuels@gmail.com>
2025-03-01 18:16:45 +0100

    Split run.sh out of start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-$(dirname `readlink -f $BASH_SOURCE`)/config.sh || (echo "Failed configuration" ; exit)
-
-cd /srv/ledgersmb
-LSMB_CONFIG_FILE=${LSMB_CONFIG_FILE:-./local/conf/ledgersmb.yaml}
-export LSMB_CONFIG_FILE
-echo '--------- LEDGERSMB CONFIGURATION:  ledgersmb.conf'
-cat ${LSMB_CONFIG_FILE}
-echo '--------- LEDGERSMB CONFIGURATION --- END'
-
-# ':5762:' suppresses an uninitialized variable warning in starman
-# the last colon means "don't connect using tls"; without it, there's a warning
-exec starman --listen 0.0.0.0:5762 --workers ${LSMB_WORKERS:-5} \
-             -I lib -I old/lib \
-             --preload-app bin/ledgersmb-server.psgi
+home_dir="$(dirname `readlink -f $BASH_SOURCE`)"
+"$home_dir/config.sh" || (echo "Failed configuration" ; exit)
+exec "$home_dir/run.sh"
