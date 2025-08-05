0690e55c43310297e06d5789df14312b218bb5ed
Erik Huelsmann <ehuels@gmail.com>
2025-08-05 11:46:57 +0200

    Move last 'config' responsibility into config.sh

--- config.sh
+++ config.sh
@@ -159,4 +159,10 @@ EOF
   fi
 fi
 
+LSMB_CONFIG_FILE=${LSMB_CONFIG_FILE:-./local/conf/ledgersmb.yaml}
+export LSMB_CONFIG_FILE
+echo "--------- LEDGERSMB CONFIGURATION:  $LSMB_CONFIG_FILE"
+cat ${LSMB_CONFIG_FILE}
+echo '--------- LEDGERSMB CONFIGURATION --- END'
+
 exit 0
