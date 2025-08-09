7af822bd9ca5d6fe80b8906805aaa36996ed06f4
Erik Huelsmann <ehuels@gmail.com>
2025-08-09 17:05:01 +0200

    Quote configuration file name

--- base/start.sh
+++ base/start.sh
@@ -3,10 +3,10 @@
 home_dir="$(dirname $(readlink -f $BASH_SOURCE))"
 "$home_dir/config.sh" || { echo "Failed configuration" ; exit 1 }
 
-LSMB_CONFIG_FILE=${LSMB_CONFIG_FILE:-/srv/ledgersmb/local/conf/ledgersmb.yaml}
+LSMB_CONFIG_FILE="${LSMB_CONFIG_FILE:-/srv/ledgersmb/local/conf/ledgersmb.yaml}"
 export LSMB_CONFIG_FILE
 echo "--------- LEDGERSMB CONFIGURATION:  $LSMB_CONFIG_FILE"
-cat ${LSMB_CONFIG_FILE}
+cat "${LSMB_CONFIG_FILE}"
 echo '--------- LEDGERSMB CONFIGURATION --- END'
 
 exec "$home_dir/run.sh"
