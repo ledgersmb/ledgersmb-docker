2e314e5c5af0ca4a4513449ef03eefabc2582b7e
Matt Palmer <mpalmer@hezmatt.org>
2026-03-17 10:13:57 +1100

    Fix syntax error in base image start.sh
    Trying to run the base image gives the following error:
    
    ```
    /usr/local/bin/start.sh: line 13: syntax error: unexpected end of file
    ```
    
    The change proposed causes the script to no longer fail on parsing.

--- base/start.sh
+++ base/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 home_dir="$(dirname $(readlink -f $BASH_SOURCE))"
-"$home_dir/config.sh" || { echo "Failed configuration" ; exit 1 }
+"$home_dir/config.sh" || { echo "Failed configuration" ; exit 1; }
 
 LSMB_CONFIG_FILE="${LSMB_CONFIG_FILE:-/srv/ledgersmb/local/conf/ledgersmb.yaml}"
 export LSMB_CONFIG_FILE
